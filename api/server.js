fc459f051055db054b39bafd0776181af4fc93f6
Taylor <52665999+taylorhh5@users.noreply.github.com>
2019-11-14 15:20:39 -0500

    Server listening on port 5000

---
+++ api/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+
+const server = express();
+
+server.use(express.json());
+
+server.get('/', (req, res) => {
+  res.status(200).json({ api: 'server running' });
+});
+
+
+
+module.exports = server;
