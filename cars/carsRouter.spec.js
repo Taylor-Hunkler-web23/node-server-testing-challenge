717f936b23a84806eb9a2a1852e8a6b945d4b0be
Taylor <52665999+taylorhh5@users.noreply.github.com>
2019-11-14 17:56:18 -0500

    Created test to check for status 200 on new POST

--- cars/carsRouter.spec.js
+++ cars/carsRouter.spec.js
@@ -2,19 +2,51 @@ const request = require('supertest'); //bring in supertest
 
 const server = require('../api/server.js') //bring in server
 
+const db = require('../data/dbConfig.js')
+
+const cars = require('./cars-model.js')
 
 describe('cars router', function () {
     describe('get', function () {
         it('should return 200 OK', function () {
 
-            return request(server) 
-                .get('/cars')    
+            return request(server)
+                .get('/cars')
                 .then(res => {
 
-                    expect(res.status).toBe(200); 
+                    expect(res.status).toBe(200);
                 })
         })
     })
+
+
+
 })
 
 
+describe('remove()', function () {
+
+    beforeEach(async () => {
+        await db('cars').truncate();
+
+    })
+
+
+    it('should respond with status 200', async function () {
+
+        await cars.insert({ name: "Truck" });
+
+        await request(server)
+            .delete("/cars/1")
+            .then(res => {
+                expect(res.status).toBe(200)
+            })
+
+
+
+
+    });
+
+
+
+})
